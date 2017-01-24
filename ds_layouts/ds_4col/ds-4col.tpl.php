83b8f4057550598b5f4cd89a448ffc01b3117807
Joshua Li <oasislrjjrl@gmail.com>
2017-01-25 09:50:35 +1100

    [Master] Changed way to play video.

--- ds_layouts/ds_4col/ds-4col.tpl.php
+++ ds_layouts/ds_4col/ds-4col.tpl.php
@@ -10,21 +10,23 @@
 <?php endif; ?>
 
 <div class="ds-4col group-top">
-  <div class="group-top-left">
-    <div class="video__thumb video-wrapper">
-      <?php print $top_left; ?>
+  <div class="ds-wrapper">
+    <div class="group-top-left">
+      <div class="video__thumb video-wrapper">
+        <?php print $top_left; ?>
+      </div>
+      <div class="group-bottom-left">
+        <?php print $bottom_left; ?>
+      </div>
     </div>
-    <div class="group-bottom-left">
-      <?php print $bottom_left; ?>
-    </div>
-  </div>
 
-  <div class="group-top-right">
-    <div class="video__extrainfo">
-      <?php print $top_right; ?>
-    </div>
-    <div class="group-bottom-right">
-      <?php print $bottom_right; ?>
+    <div class="group-top-right">
+      <div class="video__extrainfo meta">
+        <?php print $top_right; ?>
+      </div>
+      <div class="group-bottom-right">
+        <?php print $bottom_right; ?>
+      </div>
     </div>
   </div>
 </div>
