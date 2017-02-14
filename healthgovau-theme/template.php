557ce1b90a31455648b06ce3a21bccef89e1e5a7
Joshua Li <oasislrjjrl@gmail.com>
2017-02-14 12:03:01 +1100

    [girlsmove] Added activities filter for landing page.

--- healthgovau-theme/template.php
+++ healthgovau-theme/template.php
@@ -386,6 +386,22 @@ function healthgovau_form_alter(&$form, &$form_state, $form_id) {
     // Attach character countdown JS.
     $form['#attached']['js'][] = drupal_get_path('theme', 'healthgovau') . '/js/healthgovau.feedback.js';
   }
+  
+  // Override activities exposed AJAX form.
+  if ($form_id == 'views_exposed_form' && $form['#id'] == 'views-exposed-form-activities-block') {
+    // Get all activities type tids.
+    $options = $form['field_activity_type_tid']['#options'];
+    unset($form['field_activity_type_tid']);
+    unset($form['submit']);
+    $markup = '<div class="tags"><dl><dt class="visuallyhidden">Type</dt>';
+    foreach($options as $tid => $title) {
+      $markup .= '<dd><a href="campaign/' . arg(1) . '/activities?field_activity_type_tid[]=' . $tid . '">' . $title . '</a></dd>';
+    }
+    $markup .= '</dl></div>';
+    $form['activity_links'] = array(
+      '#markup' => $markup,
+    );
+  }
 }
 
 /**
