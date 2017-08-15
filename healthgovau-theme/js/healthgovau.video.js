2e27ccb1d5d81fef4bfe33f431cb15241a0dd66b
Danny <dannymarlow78@hotmail.com>
2017-08-15 16:58:06 +1000

    Added enablejsapi=1 to YouTube video embed to permit Tag Manage event logging.

--- healthgovau-theme/js/healthgovau.video.js
+++ healthgovau-theme/js/healthgovau.video.js
@@ -9,7 +9,7 @@
         e.preventDefault();
         var videoid = $(this).attr("data-youtubeid");
         // Create an iFrame with autoplay set to true
-        var iframe_url = "https://www.youtube.com/embed/" + videoid + "?autoplay=1&autohide=2&border=0&wmode=opaque&rel=0&html5=1&fs=1";
+        var iframe_url = "https://www.youtube.com/embed/" + videoid + "?autoplay=1&enablejsapi=1&autohide=2&border=0&wmode=opaque&rel=0&html5=1&fs=1";
         if ($(this).data('params')) iframe_url += '&' + $(this).data('params');
         // The height and width of the iFrame should be the same as parent
         var iframe = $('<iframe/>', { 'title': 'YouTube video player', 'id': 'youtube-iframe', 'allowfullscreen': 'allowfullscreen', 'frameborder': '0', 'aria-live': 'assertive', 'src': iframe_url });
