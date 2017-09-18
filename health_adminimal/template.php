ee31ec66f2eeca8bfab6069ea44288093304e509
Dylan Rolfe <dylan.rolfe@health.gov.au>
2017-09-18 14:45:22 +1000

    [HT-224-admin-ui] Collapsing formatting options. Fixing some styling issues with paragraphs. Dividers between draggable rows. Moving field help after label but before input.

--- health_adminimal/template.php
+++ health_adminimal/template.php
@@ -51,3 +51,113 @@ function health_adminimal_form_alter(&$form) {
 
   }
 }
+
+
+/**
+ * Implements theme_fieldset().
+ */
+function health_adminimal_fieldset($variables) {
+  $element = $variables['element'];
+
+  // Make the information below a wysiwyg in a collapsible fieldset.
+  if (key_exists('guidelines', $element)) {
+    $element['#title'] = 'Formatting information';
+    $element['#attributes']['class'][] = 'collapsible';
+    $element['#attributes']['class'][] = 'collapsed';
+  }
+
+  element_set_attributes($element, array('id'));
+  _form_set_class($element, array('form-wrapper'));
+
+  $output = '<fieldset' . drupal_attributes($element['#attributes']) . '>';
+  if (!empty($element['#title'])) {
+    // Always wrap fieldset legends in a SPAN for CSS positioning.
+    $output .= '<legend><span class="fieldset-legend">' . $element['#title'] . '</span></legend>';
+  }
+  $output .= '<div class="fieldset-wrapper">';
+  if (!empty($element['#description'])) {
+    $output .= '<div class="fieldset-description">' . $element['#description'] . '</div>';
+  }
+  $output .= $element['#children'];
+  if (isset($element['#value'])) {
+    $output .= $element['#value'];
+  }
+  $output .= '</div>';
+  $output .= "</fieldset>\n";
+  return $output;
+}
+
+
+
+/**
+ * Implements theme_form_element().
+ */
+function health_adminimal_form_element($variables) {
+  $element = &$variables['element'];
+
+  // This function is invoked as theme wrapper, but the rendered form element
+  // may not necessarily have been processed by form_builder().
+  $element += array(
+    '#title_display' => 'before',
+  );
+
+  // Add element #id for #type 'item'.
+  if (isset($element['#markup']) && !empty($element['#id'])) {
+    $attributes['id'] = $element['#id'];
+  }
+  // Add element's #type and #name as class to aid with JS/CSS selectors.
+  $attributes['class'] = array('form-item');
+  if (!empty($element['#type'])) {
+    $attributes['class'][] = 'form-type-' . strtr($element['#type'], '_', '-');
+  }
+  if (!empty($element['#name'])) {
+    $attributes['class'][] = 'form-item-' . strtr($element['#name'], array(' ' => '-', '_' => '-', '[' => '-', ']' => ''));
+  }
+  // Add a class for disabled elements to facilitate cross-browser styling.
+  if (!empty($element['#attributes']['disabled'])) {
+    $attributes['class'][] = 'form-disabled';
+  }
+  $output = '<div' . drupal_attributes($attributes) . '>' . "\n";
+
+  // If #title is not set, we don't display any label or required marker.
+  if (!isset($element['#title'])) {
+    $element['#title_display'] = 'none';
+  }
+  $prefix = isset($element['#field_prefix']) ? '<span class="field-prefix">' . $element['#field_prefix'] . '</span> ' : '';
+  $suffix = isset($element['#field_suffix']) ? ' <span class="field-suffix">' . $element['#field_suffix'] . '</span>' : '';
+
+  switch ($element['#title_display']) {
+    case 'before':
+    case 'invisible':
+      $output .= ' ' . theme('form_element_label', $variables);
+      // Help text.
+      if (!empty($element['#description'])) {
+        $output .= '<div class="description">' . $element['#description'] . "</div>\n";
+      }
+      $output .= ' ' . $prefix . $element['#children'] . $suffix . "\n";
+      break;
+
+    case 'after':
+      $output .= ' ' . $prefix . $element['#children'] . $suffix;
+      $output .= ' ' . theme('form_element_label', $variables) . "\n";
+      // Help text.
+      if (!empty($element['#description'])) {
+        $output .= '<div class="description">' . $element['#description'] . "</div>\n";
+      }
+      break;
+
+    case 'none':
+    case 'attribute':
+      // Help text.
+      if (!empty($element['#description'])) {
+        $output .= '<div class="description">' . $element['#description'] . "</div>\n";
+      }
+      // Output no label and no required marker, only the children.
+      $output .= ' ' . $prefix . $element['#children'] . $suffix . "\n";
+      break;
+  }
+
+  $output .= "</div>\n";
+
+  return $output;
+}
