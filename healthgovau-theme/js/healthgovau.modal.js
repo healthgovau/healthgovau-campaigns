e1c846594aec6fa59d6f5e37b10608b44fea7a54
Danny <dannymarlow78@hotmail.com>
2018-03-02 11:20:38 +1100

    Added cookie read and write

--- healthgovau-theme/js/healthgovau.modal.js
+++ healthgovau-theme/js/healthgovau.modal.js
@@ -2,35 +2,30 @@
   Drupal.behaviors.modal = {
     attach: function (context, settings) {
 
-      // Add modal effect. Check http://refills.bourbon.io/components
-      $(".modal-state").on("change", function() {
-        if ($(this).is(":checked")) {
-          $("body").addClass("modal-open");
-        } else {
-          $("body").removeClass("modal-open");
-        }
-      });
-
+      var surveyID = 2;
+      
       $(".modal-fade-screen, .modal-close").on("click", function() {
         $("body").removeClass("modal-open");
+        Cookies.set('survey-dismiss', surveyID);
       });
 
       $(".modal-inner").on("click", function(e) {
         e.stopPropagation();
       });
 
-      var minNumber = 1;
-      var maxNumber = 2;
-      // Specify the random number max
-      
-      var randomNumber = randomNumberFromRange(minNumber, maxNumber);
-      console.log(randomNumber);
-      function randomNumberFromRange(min, max) {
-        return Math.floor(Math.random() * (max - min + 1) + min);
-      }
+      if(Cookies.get('survey-dismiss') != surveyID){
 
-      if (randomNumber == 2) {
-        $("body").addClass("modal-open");
+        var minNumber = 1;
+        var maxNumber = 5; // every nth page laod if no cookie
+
+        var randomNumber = randomNumberFromRange(minNumber, maxNumber);
+        function randomNumberFromRange(min, max) {
+          return Math.floor(Math.random() * (max - min + 1) + min);
+        }
+
+        if (randomNumber == 2) {
+          $("body").addClass("modal-open");
+        }
       }
 
     }
