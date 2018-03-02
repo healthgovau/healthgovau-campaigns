5b516011a2271659d7d036b7d4abc358a632ba4a
Danny <dannymarlow78@hotmail.com>
2018-03-02 14:56:49 +1100

    Write cookie on taking the survey too!

--- healthgovau-theme/templates/survey.tpl.php
+++ healthgovau-theme/templates/survey.tpl.php
@@ -4,7 +4,7 @@
       <div class="modal-close" for="modal-1"></div>
       <h2>Do you have a moment?</h2>
       <p class="modal-intro">We'd like to ask you 5 quick questions about what brought you here today.</p>
-      <p class="modal-content"><a class="see-more" href="https://surveys.globaltestmarket.com/survey/selfserve/1aab/1802156?_dj#?" target="_blank">Take the survey</a> <a class="see-more modal-close-link">No thanks</a></p>
+      <p class="modal-content"><a class="see-more modal-close-link" href="https://surveys.globaltestmarket.com/survey/selfserve/1aab/1802156?_dj#?" target="_blank">Take the survey</a> <a class="see-more modal-close-link">No thanks</a></p>
     </div>
   </div>
 </div>
