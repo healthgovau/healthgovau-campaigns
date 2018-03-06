7f02b1ba1f85ede373b7324d740a647a4f83eca2
Danny <dannymarlow78@hotmail.com>
2018-03-06 12:28:36 +1100

    Updated Qualtrics survey code with provided script and ID

--- healthgovau-theme/template.php
+++ healthgovau-theme/template.php
@@ -258,12 +258,12 @@ function healthgovau_preprocess_node(&$variables) {
   $variables['immunisation_survey'] = '';
   if ($node->nid == '931') {
     drupal_add_js(drupal_get_path('theme', 'healthgovau') . '/js/immunisation-survey.js');
-    $variables['immunisation_survey'] = '<div id=\'SI_e9tKdVZvk8qGE0B\'><!--DO NOT REMOVE-CONTENTS PLACED HERE--></div>';
+    $variables['immunisation_survey'] = '<div id=\'SI_9mpXcehBBbiUe6V\'><!--DO NOT REMOVE-CONTENTS PLACED HERE--></div>';
   }
   elseif (isset($node->field_campaign[LANGUAGE_NONE])) {
     if ($node->field_campaign[LANGUAGE_NONE][0]['target_id'] == '931') {
       drupal_add_js(drupal_get_path('theme', 'healthgovau') . '/js/immunisation-survey.js');
-      $variables['immunisation_survey'] = '<div id=\'SI_e9tKdVZvk8qGE0B\'><!--DO NOT REMOVE-CONTENTS PLACED HERE--></div>';
+      $variables['immunisation_survey'] = '<div id=\'SI_9mpXcehBBbiUe6V\'><!--DO NOT REMOVE-CONTENTS PLACED HERE--></div>';
     }
   }
 
